891aaf6bea9c00cec4dd0f8252c1f388b726a14a
tjdgns5272 <69573484+tjdgns5272@users.noreply.github.com>
2021-07-01 22:57:24 +0900

    solved bfs

--- Toy/treeBFS.js
+++ Toy/treeBFS.js
@@ -1,17 +1,17 @@
 let bfs = function (node) {
+
+    let queue = [node];
     let result = []
 
-    result.push(node.value)
+    while (queue.length > 0) {
+        //Queue를 사용하여 트리의 노드들을 레벨별로 탐색하자.
+        // 반복문을 통해 queue 안에 있는 노드들을 하나씩 탐색하고 자식이 있다면 queue에 넣어보자.
+        //queue 안에 아무 것도 남지 않았다면 다 탐색한 것이기에 그때, 값을 담은 배열을 리턴하자.
+        const head = queue[0]
+        queue.shift();
+        result.push(head.value)
 
-    if(node.children) {
-        for(let child of node.children) {
-            result.push(child.value)
-        }
-        for(let child of node.children) {
-            const childNode = bfs(child)
-        }
-    } else {
-        return
+        head.children.forEach(child => queue.push(child))
     }
 
     return result
