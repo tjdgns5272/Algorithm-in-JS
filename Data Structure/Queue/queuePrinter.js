174906a585e71ba7f67b058909ef93b1b711bc35
tjdgns5272 <69573484+tjdgns5272@users.noreply.github.com>
2021-07-02 22:22:55 +0900

    fix: printer2 after study

--- Data Structure/Queue/queuePrinter.js
+++ Data Structure/Queue/queuePrinter.js
@@ -1,8 +1,9 @@
-function queuePrinter(bufferSize, capacities, documents) {
+function queuePrinter(bufferSize, capacities, documents) { // 1차시도
     let buffer = new Array(bufferSize).fill(0);
     let sec = 1;
 
     while (buffer.length !== 0) {
+
 
         let curSum = buffer.reduce((a, b) => a + b);
 
@@ -20,4 +21,72 @@ function queuePrinter(bufferSize, capacities, documents) {
     console.log(sec);
     return sec
 }
-queuePrinter(3, 10, [7, 4, 7])
+
+
+function queuePrinter2(bufferSize, capacities, documents) { // 2차시도 (before algorithm study)
+    let buffer = new Array(bufferSize).fill(0);  // [0,0]
+    let sec = 0;
+
+    const enqueue = (el) => buffer.push(el);
+    const dequeue = (arr) => arr.shift();
+    if(buffer.length === 1) return sec+documents.length
+    while (documents.length !== 0) {
+        console.log(sec, buffer, documents);
+
+        let curSize = buffer.slice(1).reduce((a, b) => a + b); // 현재 용량
+
+        if (curSize + documents[0] <= capacities) { // 현재 용량 + 들어올 문서 합이 한도보다 작을때
+            enqueue(documents[0]);
+            dequeue(documents);
+        } else {                                        // 클때
+            enqueue(0)
+        }
+
+        dequeue(buffer)
+
+        sec++;
+    }
+    console.log(sec);
+    return sec
+}
+
+queuePrinter2(1, 10, [5, 5, 5, 5])
+
+
+function queuePrinter3(bufferSize, capacities, documents) { // reference
+    let count = 0;
+    let queue = [];
+    let totalBufferVolume = 0;
+
+      for(let i = 0; i < bufferSize; i++){
+        queue.push(0);
+    }
+
+    let currentDocument = documents.shift();
+
+    queue.unshift(currentDocument);
+    queue.pop();
+
+    totalBufferVolume = totalBufferVolume + currentDocument;
+
+    count++;
+    while(totalBufferVolume){
+
+        totalBufferVolume = totalBufferVolume - queue.pop();
+
+        currentDocument = documents.shift();
+
+        if(currentDocument + totalBufferVolume <= capacities){
+
+            queue.unshift(currentDocument);
+            totalBufferVolume = totalBufferVolume + currentDocument;
+
+        }else{
+
+            documents.unshift(currentDocument);
+            queue.unshift(0);
+        }
+        count++;
+    }
+    return count;
+}
