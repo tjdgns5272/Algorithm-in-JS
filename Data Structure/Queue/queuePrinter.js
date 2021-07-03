9503b2746a3f3e175cba14ac81ac585b6863d0de
tjdgns5272 <69573484+tjdgns5272@users.noreply.github.com>
2021-07-03 20:30:33 +0900

    Solved problem completely

--- Data Structure/Queue/queuePrinter.js
+++ Data Structure/Queue/queuePrinter.js
@@ -29,7 +29,7 @@ function queuePrinter2(bufferSize, capacities, documents) { // 2차시도 (befor
 
     const enqueue = (el) => buffer.push(el);
     const dequeue = (arr) => arr.shift();
-    if(buffer.length === 1) return sec+documents.length
+    if (buffer.length === 1) return sec + documents.length
     while (documents.length !== 0) {
         console.log(sec, buffer, documents);
 
@@ -50,15 +50,12 @@ function queuePrinter2(bufferSize, capacities, documents) { // 2차시도 (befor
     return sec
 }
 
-queuePrinter2(1, 10, [5, 5, 5, 5])
-
-
 function queuePrinter3(bufferSize, capacities, documents) { // reference
     let count = 0;
     let queue = [];
     let totalBufferVolume = 0;
 
-      for(let i = 0; i < bufferSize; i++){
+    for (let i = 0; i < bufferSize; i++) {
         queue.push(0);
     }
 
@@ -70,18 +67,18 @@ function queuePrinter3(bufferSize, capacities, documents) { // reference
     totalBufferVolume = totalBufferVolume + currentDocument;
 
     count++;
-    while(totalBufferVolume){
+    while (totalBufferVolume) {
 
         totalBufferVolume = totalBufferVolume - queue.pop();
 
         currentDocument = documents.shift();
 
-        if(currentDocument + totalBufferVolume <= capacities){
+        if (currentDocument + totalBufferVolume <= capacities) {
 
             queue.unshift(currentDocument);
             totalBufferVolume = totalBufferVolume + currentDocument;
 
-        }else{
+        } else {
 
             documents.unshift(currentDocument);
             queue.unshift(0);
@@ -90,3 +87,34 @@ function queuePrinter3(bufferSize, capacities, documents) { // reference
     }
     return count;
 }
+
+function queuePrinter4(bufferSize, capacities, documents) {
+    let sec = 0;         // 4, 10, [7,4,7,4]
+    let buffer = new Array(bufferSize).fill(0)
+    let curSize = 0;
+
+    let curDoc = documents.shift() // 현재 처리할 문서
+
+    buffer.push(curDoc)   // [0,0,0,0,7]
+    buffer.shift() // [0,0,0,7]
+    curSize += curDoc  //  현재 용량 업데이트
+    sec++
+    console.log(sec, buffer, documents);
+
+    while (curSize) {
+        curDoc = documents.shift() // 현재 처리할 문서
+        curSize -= buffer.shift() // 현재 사이즈에서 빠질 내용은 빼줌
+
+        if (curSize + curDoc <= capacities) {  // 현재 사이즈 + 처리할 문서 합 <= 최대사이즈
+            buffer.push(curDoc) // 버퍼 배열에 추가해주고
+            curSize += curDoc   // 현재 사이즈에 처리문서를 더해줌
+        } else {
+            documents.unshift(curDoc)
+            buffer.push(0)
+        }
+        sec++
+    }
+    return sec
+}
+
+console.log(queuePrinter4(4, 10, [7, 4, 7, 4]))
