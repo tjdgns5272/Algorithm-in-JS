7e46ab3c3a077a447c2c90b8edf2e6324a5debbb
tjdgns5272 <69573484+tjdgns5272@users.noreply.github.com>
2021-07-25 21:26:44 +0900

    Solved LSCS in DP

--- Toy/LSCS.js
+++ Toy/LSCS.js
@@ -1,9 +1,33 @@
-const LSCS = function (arr) {
-  //일단 양수면 무조건 더함
-  // 음수이면 그 뒤에 음수가 나올때 까지의 합보다 작으면 더함
-  const result = []
-  arr.forEach((num, idx, origin) => {
-    num
-  })
+const LSCS1 = function (arr) { // O(n^2)
+  let max = -100000;
+  for (let i = 0; i < arr.length; i++) {
+    let sum = arr[i];
+    if (sum > max) max = sum;
+    for (let j = i + 1; j < arr.length; j++) {
+      sum = sum + arr[j];
+      if (sum > max) max = sum;
+    }
+  }
+  return max;
+
 };
-LSCS([1, 2, 3, -4, 5, -4, 5, -4, -4, -1]) // 8
+
+const LSCS2 = function (arr) { // O(n) --> DP
+  let arrSum = arr[0]
+  let max = arr[0]
+
+  // 쭉 더해서 음수인 부분은 굳이 더할 필요가 없다.
+  arr.forEach((num,idx) => {
+    if(idx>0){
+      // subArrSum는 바로 직전의 요소까지 검토했을 때 가장 연속합
+      // 연속합에 추가로 검토하는 요소, 즉 arr[i]를 더하는 것보다
+      // arr[i] 하나의 값이 더 큰 경우 (subArrSum가 음수일 경우)
+      // subArrSum를 버리는 게 좋다.
+      arrSum = Math.max(num, num+arrSum);
+
+      max = Math.max(max, arrSum)
+    }
+  })
+  return max
+}
+
