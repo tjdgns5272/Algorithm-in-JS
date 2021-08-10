1b97355d7b49d8633591f876195fc7f0f5f20f7c
tjdgns5272 <69573484+tjdgns5272@users.noreply.github.com>
2021-08-10 21:47:05 +0900

    Reviewed after study meeting

--- Toy/getItemFromTwoSortedArrays.js
+++ Toy/getItemFromTwoSortedArrays.js
@@ -7,7 +7,7 @@ const getItemFromTwoSortedArrays = function (arr1, arr2, k) {
 
   const sortedArr = [...result, ...arr1, ...arr2]; // 아래 세줄과 같은 역할을 하는 코드
   console.log(sortedArr);
-  return sortedArr[k-1]
+  return sortedArr[k - 1]
 
   // while (startIndex <= endIndex) {
   //   const midIndex = Math.floor((startIndex + endIndex) / 2)
@@ -22,37 +22,33 @@ const getItemFromTwoSortedArrays2 = function (arr1, arr2, k) {
 
   while (k > 0) {
     // 이진 탐색을 위해 각 배열에서 k를 절반으로 쪼개서 카운트 한다.
-    let cnt = Math.ceil(k / 2);
-    let leftStep = cnt,
-      rightStep = cnt;
+    let step = Math.ceil(k / 2);
 
     // 엣지 케이스
     // 카운트가 남았음에도 배열의 끝에 도달하면 k를 나머지 배열쪽으로 넘긴다.
     if (leftIdx === arr1.length) {
-      rightIdx = rightIdx + k;
+      rightIdx += k;
       break;
     }
-
     if (rightIdx === arr2.length) {
-      leftIdx = leftIdx + k;
+      leftIdx += k;
       break;
     }
-
     // 엣지 케이스
-    // 현재 카운트가 남아있는 후보 요소들보다 많을 경우, leftStep(현재 할당량)을 남아있는 요소들의 개수로 바꾼다.
-    if (cnt > arr1.length - leftIdx) leftStep = arr1.length - leftIdx;
-    if (cnt > arr2.length - rightIdx) rightStep = arr2.length - rightIdx;
+    // 현재 카운트가 남아있는 후보 요소들보다 많을 경우, step(현재 할당량)을 남아있는 요소들의 개수로 바꾼다.
+    if (step > arr1.length - leftIdx) step = arr1.length - leftIdx;
+    if (step > arr2.length - rightIdx) step = arr2.length - rightIdx;
 
     // 두 배열의 현재 검사 요소 위치를 비교해서, 그 값이 작은 배열은 비교한 위치 앞에 있는 요소들을 모두 후보군에서 제외시킨다.
-    const value1 = arr1[leftIdx + leftStep - 1]
-    const value2 = arr2[rightIdx + rightStep - 1]
+    const value1 = arr1[leftIdx + step - 1]
+    const value2 = arr2[rightIdx + step - 1]
     if (value1 < value2) {
-      leftIdx += leftStep;
+      leftIdx += step;
       // 처리가 끝나면 k값을 절반으로 떨어뜨린다.
-      k -= leftStep;
+      k -= step;
     } else {
-      rightIdx += rightStep;
-      k -= rightStep;
+      rightIdx += step;
+      k -= step;
     }
   }
 
@@ -62,4 +58,4 @@ const getItemFromTwoSortedArrays2 = function (arr1, arr2, k) {
   return Math.max(leftMax, rightMax);
 };
 
-console.log(getItemFromTwoSortedArrays2([1, 2, 3, 4], [2, 4, 6, 8, 10], 5));
+console.log(getItemFromTwoSortedArrays2([1, 20], [2, 13, 14, 16, 19], 7));
--- Toy/robotPath.js
+++ Toy/robotPath.js
@@ -1,11 +1,6 @@
 const robotPath = function (room, src, dst) {
   // TODO: 여기에 코드를 작성합니다.
-  const DIR = {
-    'U': [-1, 0],
-    'D': [1, 0],
-    'L': [0, -1],
-    'R': [0, 1]
-  }
+
   const M = room.length // 세로 길이
   const N = room[0].length // 가로 길이
   const isValid = (x, y) => {   // 범위안에 있는지 검사하는 함수
@@ -33,7 +28,42 @@ const robotPath = function (room, src, dst) {
 
   //출발지점 부터 시작해서 인접행렬 (상하좌우)가 0인 경
 };
+const robotPath2 = function (room, src, dst) {
 
+  const rowLen = room[0].length
+  const colLen = room.length
+  room[src[0]][src[1]] = 1;   // 시작점을 1로 만들어준다
+
+  let curPoint = [src];
+  const DIR = [
+    [-1, 0], // up
+    [1, 0], // down
+    [0, 1], // right
+    [0, -1] // left
+  ]
+  //범위 안에 드는지 검사하는 함수
+  const isValid = (M, N) => (0 <= M  && M < colLen) && (0 <= N && N < rowLen)
+
+  const movePoint = function (point) {
+    const [pointCol, pointRow] = point;
+    for (let i = 0; i < 4; i++) {
+      let [dy, dx] = DIR[i]
+      dy += pointCol // 이동 후 dy 좌표
+      dx += pointRow // 이동 후 dx 좌표
+      if (isValid(dy, dx) && room[dy][dx]===0) { // 범위 안에 들면서 방문한적이 없는 경우
+        curPoint.push([dy, dx])  // 큐 안에 넣어준다
+        room[dy][dx] = room[pointCol][pointRow] + 1; // 이동 전 숫자에 1을 더한 값을 할당해준다
+      }
+    }
+  }
+
+  while (curPoint.length > 0) {  // 큐의 길이가 0이 될때 까지 반복
+    let point = curPoint.shift();
+    movePoint(point);
+  }
+
+  return room[dst[0]][dst[1]]-1;  // 출발지점을 1로 지정해줘서 맨 마지막에는 -1을 해줌
+};
 const room = [
   [1, 1, 0, 1],
   [0, 1, 0, 1],
@@ -41,5 +71,5 @@ const room = [
 ];
 const src = [1, 0];
 const dst = [1, 2];
-console.log(robotPath(room, src, dst))
+console.log(robotPath2(room, src, dst))
 
